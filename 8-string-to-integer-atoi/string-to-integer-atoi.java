9101850b49ab887e10e8d528b1b6885de1ce2961
abhishekkumar68 <150378598+abhishekkumar68@users.noreply.github.com>
2026-05-09 02:03:22 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (58.86%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,11 +1,41 @@
 class Solution {
     public int myAtoi(String s) {
+        // int i = 0;
+        // int n = s.length();
+
+        // while(i<n && s.charAt(i)==' '){
+        //     i++;
+        // }
+
+        // int sign = 1;
+        // if(i<n && (s.charAt(i)=='+' || s.charAt(i)=='-')){
+        //     if(s.charAt(i)=='-'){
+        //         sign = -1;
+        //     }
+        //     i++;
+        // }
+
+        // long ans = 0;
+        // while(i<n && Character.isDigit(s.charAt(i))){
+        //     ans = ans*10 + (s.charAt(i) - '0');
+
+        //     if(sign*ans > Integer.MAX_VALUE){
+        //         return Integer.MAX_VALUE;
+        //     }
+
+        //     if(sign*ans < Integer.MIN_VALUE){
+        //         return Integer.MIN_VALUE;
+        //     }
+        //     i++;
+        // }
+
+        // return (int)(sign*ans);
+
+
+        // Recursion Code
         int i = 0;
         int n = s.length();
-
-        while(i<n && s.charAt(i)==' '){
-            i++;
-        }
+        while(i<n && s.charAt(i)==' ')i++;
 
         int sign = 1;
         if(i<n && (s.charAt(i)=='+' || s.charAt(i)=='-')){
@@ -15,20 +45,21 @@ class Solution {
             i++;
         }
 
-        long ans = 0;
-        while(i<n && Character.isDigit(s.charAt(i))){
-            ans = ans*10 + (s.charAt(i) - '0');
+        return helper(s, i, 0, sign);
+    }
 
-            if(sign*ans > Integer.MAX_VALUE){
-                return Integer.MAX_VALUE;
-            }
-
-            if(sign*ans < Integer.MIN_VALUE){
-                return Integer.MIN_VALUE;
-            }
-            i++;
+    int helper(String s, int i, long ans, int sign){
+        int n = s.length();
+        if(i>=n || !Character.isDigit(s.charAt(i))){
+            return (int)(sign*ans);
         }
 
-        return (int)(sign*ans);
+        ans = ans*10 + (s.charAt(i)-'0');
+
+        if(sign*ans > Integer.MAX_VALUE)return Integer.MAX_VALUE;
+        if(sign*ans < Integer.MIN_VALUE)return Integer.MIN_VALUE;
+
+        return helper(s, i+1, ans, sign);
+
     }
 }
