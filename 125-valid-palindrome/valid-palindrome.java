84b3bb135f179038658df1c720d16b41e2caa603
abhishekkumar68 <150378598+abhishekkumar68@users.noreply.github.com>
2026-05-15 17:46:29 +0530

    Time: 2 ms (99.33%) | Memory: 44.4 MB (65.06%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,20 +1,40 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        String str = "";
-        for(int i=0;i<s.length();i++){
-            char ch = Character.toLowerCase(s.charAt(i));
-            if(Character.isLetterOrDigit(ch)){
-                str+=ch;
-            }
-        }
+        // String str = "";
+        // for(int i=0;i<s.length();i++){
+        //     char ch = Character.toLowerCase(s.charAt(i));
+        //     if(Character.isLetterOrDigit(ch)){
+        //         str+=ch;
+        //     }
+        // }
 
-        int j=str.length()-1;
-        for(int i=0;i<str.length()/2;i++){
-            if(str.charAt(i)!=str.charAt(j)){
-                return false;
+        // int j=str.length()-1;
+        // for(int i=0;i<str.length()/2;i++){
+        //     if(str.charAt(i)!=str.charAt(j)){
+        //         return false;
+        //     }
+        //     j--;
+        // }
+        // return true;
+
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<j){
+            if(!Character.isLetterOrDigit(s.charAt(i))) i++;
+            else if(!Character.isLetterOrDigit(s.charAt(j))) j--;
+            else{
+                int left = Character.toLowerCase(s.charAt(i));
+                int right = Character.toLowerCase(s.charAt(j));
+
+                if(left!=right){
+                    return false;
+                }
+
+                i++;
+                j--;
             }
-            j--;
         }
         return true;
     }
