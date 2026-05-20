3300378107b797876d0ab802fade270e54dcb2ed
abhishekkumar68 <150378598+abhishekkumar68@users.noreply.github.com>
2026-05-20 18:57:47 +0530

    Time: 5 ms (46.74%) | Memory: 47.2 MB (85.85%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -1,19 +1,45 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        // int[] ans = new int[A.length];
+
+        // for(int i=0;i<A.length;i++){
+        //     int count = 0;
+        //     for(int j=0;j<=i;j++){
+        //         for(int k=0;k<=i;k++){
+        //             if(A[j]==B[k]){
+        //                 count++;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     ans[i] = count;
+        // }
+        // return ans;
+
         int[] ans = new int[A.length];
+        HashSet<Integer> setA =  new HashSet<>();
+        HashSet<Integer> setB =  new HashSet<>();
+        int common = 0;
 
         for(int i=0;i<A.length;i++){
-            int count = 0;
-            for(int j=0;j<=i;j++){
-                for(int k=0;k<=i;k++){
-                    if(A[j]==B[k]){
-                        count++;
-                        break;
-                    }
+            if(A[i]==B[i]){
+                common++;
+            }
+            else{
+                if(setB.contains(A[i])){
+                    common++;
+                }
+                if(setA.contains(B[i])){
+                    common++;
                 }
             }
-            ans[i] = count;
+
+            setA.add(A[i]);
+            setB.add(B[i]);
+            ans[i]=common;
         }
+
         return ans;
+
     }
 }
