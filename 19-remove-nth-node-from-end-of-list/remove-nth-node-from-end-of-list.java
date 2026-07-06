a5426131c9f020374c2326bee9d918af1ce1dcde
abhishekkumar68 <150378598+abhishekkumar68@users.noreply.github.com>
2026-07-06 15:50:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (76.66%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,23 +10,61 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode temp = head;
-        int length = 0;
-        while(temp!=null){
-            length++;
-            temp = temp.next;
-        } 
-        if(length==n) return head.next;
-        int prevNode = length-n;
-        temp = head;
-        while(temp!=null){
-            prevNode--;
+        // ListNode temp = head;
+        // int length = 0;
+        // while(temp!=null){
+        //     length++;
+        //     temp = temp.next;
+        // } 
 
-            if(prevNode==0) break;
-            temp = temp.next;
-        }
+        // if(length==n) return head.next;
+
+        // int prevNode = length-n;
+        // temp = head;
+        // while(temp!=null){
+        //     prevNode--;
+
+        //     if(prevNode==0) break;
+        //     temp = temp.next;
+        // }
         
-        temp.next  = temp.next.next;
+        // temp.next  = temp.next.next;
+        // return head;
+
+        // ListNode dummy = new ListNode(0);
+        // dummy.next = head;
+        // ListNode slow = dummy;
+        // ListNode fast = dummy;
+
+        // if(head.next==null) return null;
+
+        // for(int i=1;i<=n+1;i++){
+        //     fast = fast.next;
+        // }
+
+        // while(fast!=null){
+        //     slow = slow.next;
+        //     fast = fast.next;
+        // }
+
+        // slow.next = slow.next.next;
+        // return head;
+
+        ListNode slow=head;
+        ListNode fast=head;
+
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+
+        if(fast==null) return head.next;
+
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        slow.next=slow.next.next;
+
         return head;
     }
 }
