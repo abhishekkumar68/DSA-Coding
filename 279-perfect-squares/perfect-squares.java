84d56174a7bc5384cffef55da97301e03cb811e8
abhishekkumar68 <150378598+abhishekkumar68@users.noreply.github.com>
2026-07-15 10:05:40 +0530

    Time: 63 ms (29.50%) | Memory: 44.5 MB (53.08%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
+    }
+
+    public int helper(int n, int[] dp){
+        if(n==0 || n==1) return n;
+        if(dp[n]!=-1) return dp[n];
+
+        int minimum = Integer.MAX_VALUE;
+        for(int i=1;i*i<=n;i++){
+            int temp = helper(n-i*i,dp);
+            minimum = Math.min(minimum, temp);
+        }
+
+        dp[n] = minimum+1;
+        return minimum+1;
+    }
+}
